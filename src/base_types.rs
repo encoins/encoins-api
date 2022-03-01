1c951d0b81e7754f121ba0938abe7425cd9b82e7
avincigu <arthur.vinciguerra@ens-lyon.fr>
2022-03-01 16:51:32 +0100

    - added Transaction constructor

--- src/base_types.rs
+++ src/base_types.rs
@@ -22,13 +22,13 @@ pub type SeqId = u32;
 pub struct Transaction
 {
     /// seq_id is the id of the transaction. For a transaction t, seq_id will be the number of validated transfers outgoing form the sender +1.
-    pub(crate) seq_id: SeqId,
+    seq_id: SeqId,
     /// the user id of the transaction's sender
-    pub(crate) sender_id: UserId,
+    sender_id: UserId,
     /// the user id of the transaction's receiver
-    pub(crate) receiver_id: UserId,
+    receiver_id: UserId,
     /// the currency exchanged
-    pub(crate) amount: Currency
+    amount: Currency
 }
 
 
@@ -71,6 +71,20 @@ impl UserId
     pub fn get_id(&self) -> ComprPubKey
     {
         self.id
+    }
+}
+
+impl Transaction
+{
+    pub fn from(seq_id : SeqId, sender_id : UserId, receiver_id : UserId, amount : Currency) -> Transaction
+    {
+        Transaction
+        {
+            seq_id,
+            sender_id,
+            receiver_id,
+            amount
+        }
     }
 }
 
